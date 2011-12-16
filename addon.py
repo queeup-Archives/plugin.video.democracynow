970433b56718d693c6b7c0c212e50c5461c41473
queeup <ozgur.baskin@gmail.com>
2011-12-16 16:13:53 +0200

    version 1.0.3: changed : Now just todays news. Headlines, Features and Full Show.

--- addon.py
+++ addon.py
@@ -1,23 +1,56 @@
 # -*- coding: utf-8 -*-
 
 # Imports
-import os, sys, time
+import os, sys, urllib
 import xbmc, xbmcgui, xbmcplugin, xbmcaddon
+from BeautifulSoup import SoupStrainer, BeautifulSoup as BS
 
 __settings__ = xbmcaddon.Addon(id='plugin.video.democracynow')
 __icon__ = __settings__.getAddonInfo('icon')
 __fanart__ = __settings__.getAddonInfo('fanart')
-__language__ = __settings__.getLocalizedString
 
 # Fanart
 xbmcplugin.setPluginFanart(int(sys.argv[1]), __fanart__)
 
+URL = urllib.urlopen('http://m.democracynow.org/').read().replace('&quot;', '"').replace('&#8217;', "'").replace('<span class="caps">', '').replace('</span>', '')
+
 # Main
 def Main():
-  listitem_video = xbmcgui.ListItem('Video', thumbnailImage=__icon__)
-  listitem_audio = xbmcgui.ListItem('Audio', thumbnailImage=__icon__)
-  xbmcplugin.addDirectoryItem(int(sys.argv[1]), 'rss://www.democracynow.org/podcast-video.xml', listitem_video, True)
-  xbmcplugin.addDirectoryItem(int(sys.argv[1]), 'rss://www.democracynow.org/podcast.xml', listitem_audio, True)
+  soup = BS(URL, parseOnlyThese=SoupStrainer('div', 'ui-content'))
+  date = soup.find('div', 'context_header').h2.string.strip()
+  for entry in soup('li'):
+    url = entry('a', 'video_link')[0]['href']
+    #thumb = entry('a', 'video_link')[0].img['src']
+    thumb = entry('img', 'video_poster')[0]['src']
+    if not thumb.startswith("http://"):
+      thumb = 'http://m.democracynow.org' + thumb
+    title = entry('div', 'two_thirds')[0].a.string.strip()
+    if title == 'Watch':
+      title = 'Full Show'
+    elif title == 'A collection of news briefs from around the world.':
+      title = 'Headlines'
+    try:
+      summary = entry('div', 'more_summary')[0].p.string
+    except:
+      if title == 'Full Show':
+        summary = 'Watch Full Show'
+      elif title == 'Headlines':
+        summary = 'A collection of news briefs from around the world.'
+      else:
+        summary = ''
+    try:
+      duration = entry('div', 'media_icon duration')[0].string.strip().replace(' ', '').replace('m', ':').replace('s', '')
+    except:
+      duration = ''
+    listitem = xbmcgui.ListItem(title, iconImage="DefaultVideoBig.png", thumbnailImage=thumb)
+    listitem.setProperty('IsPlayable', 'true')
+    listitem.setInfo(type="video",
+                     infoLabels={"title" : title,
+                                 "label" : title,
+                                 "plot" : summary,
+                                 "duration" : duration,
+                                 "tvshowtitle" : date})
+    xbmcplugin.addDirectoryItem(int(sys.argv[ 1 ]), url, listitem, isFolder=False)
 
   xbmcplugin.setContent(int(sys.argv[1]), 'episodes')
 
