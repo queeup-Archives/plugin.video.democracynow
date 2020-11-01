4ab6ae4291f4ec44ddae6ac2d52379389fe3963f
queeup <queeup@zoho.com>
2020-11-02 00:49:03 +0300

    plugin.video.democracynow: update to v3.0.0 [new] Support kodi matrix [new] python 3 migrate

--- addon.py
+++ addon.py
@@ -6,7 +6,8 @@ import xbmcplugin
 import xbmcaddon
 import requests
 import re
-from HTMLParser import HTMLParser
+import html
+
 
 addon = xbmcaddon.Addon()
 addon_icon = addon.getAddonInfo('icon')
@@ -37,71 +38,75 @@ def remove_html_tags(text):
 
 def decode_html_entities(text):
     """Decode html entities from a string"""
-    h = HTMLParser()
-    return h.unescape(text)
+    return html.unescape(text)
 
 
 def string_correction(_str):
     return remove_html_tags(decode_html_entities(_str))
 
 
-def main():
-    for show in get_json(current_show)['media']:
-        if 'High' in show['title']:
-            url = show['src']
-            title = 'Full Show'
-            thumb = thumb_replacement
-            summary = ''
-            listitem = xbmcgui.ListItem(title, iconImage="DefaultVideoBig.png", thumbnailImage=thumb)
-            listitem.setProperty('fanart_image', addon_fanart)
-            listitem.setProperty('IsPlayable', 'true')
-            listitem.setInfo(type="video",
-                             infoLabels={"title": title,
-                                         "plot": summary})
-            xbmcplugin.addDirectoryItem(addon_handler, url, listitem, isFolder=False)
+class Main:
+    def __init__(self):
+        for show in get_json(current_show)['media']:
+            if 'High' in show['title']:
+                url = show['src']
+                title = 'Full Show'
+                thumb = thumb_replacement
+                summary = ''
+                listitem = xbmcgui.ListItem(title)
+                listitem.setInfo(type="video",
+                                 infoLabels={"title": title,
+                                             "plot": summary})
+                listitem.setArt({"icon": "DefaultVideoBig.png",
+                                 "thumb": thumb,
+                                 "fanart": addon_fanart})
+                listitem.setProperty('IsPlayable', 'true')
+                xbmcplugin.addDirectoryItem(addon_handler, url, listitem, isFolder=False)
 
-    for video in get_json(current_show)['items']:
-        if video['itemType'] == 'headline_section':
-            title = u'Headlines'
-        else:
-            title = string_correction(video['title'])
+        for video in get_json(current_show)['items']:
+            if video['itemType'] == 'headline_section':
+                title = u'Headlines'
+            else:
+                title = string_correction(video['title'])
 
-        url = ''
-        for _video in video['media']:
-            if 'High' in _video['title']:
-                url = _video['src']
+            url = None
+            for _video in video['media']:
+                if 'High' in _video['title']:
+                    url = _video['src']
+                else:
+                    pass
+
+            try:
+                thumb = video['images'][0]['url']
+            except KeyError:
+                thumb = thumb_replacement
+
+            try:
+                duration = int(video['duration'])
+            except KeyError:
+                duration = ''
+
+            try:
+                summary = string_correction(video['summary'])
+            except KeyError:
+                summary = ''
+
+            if url is not None:
+                listitem = xbmcgui.ListItem(title)
+                listitem.setInfo(type="video",
+                                 infoLabels={"title": title,
+                                             "plot": summary,
+                                             "duration": duration})
+                listitem.setArt({"icon": "DefaultVideoBig.png",
+                                 "thumb": thumb,
+                                 "fanart": addon_fanart})
+                listitem.setProperty('IsPlayable', 'true')
+                xbmcplugin.addDirectoryItem(addon_handler, url, listitem, isFolder=False)
             else:
                 pass
-
-        try:
-            thumb = video['images'][0]['url']
-        except KeyError:
-            thumb = thumb_replacement
-
-        try:
-            duration = int(video['duration'])
-        except KeyError:
-            duration = ''
-
-        try:
-            summary = string_correction(video['summary'])
-        except KeyError:
-            summary = ''
-
-        if url is not '':
-            listitem = xbmcgui.ListItem(title, iconImage="DefaultVideoBig.png", thumbnailImage=thumb)
-            listitem.setProperty('fanart_image', addon_fanart)
-            listitem.setProperty('IsPlayable', 'true')
-            listitem.setInfo(type="video",
-                             infoLabels={"title": title,
-                                         "plot": summary,
-                                         "duration": duration})
-            xbmcplugin.addDirectoryItem(addon_handler, url, listitem, isFolder=False)
-        else:
-            pass
-    xbmcplugin.setContent(addon_handler, 'episodes')
-    # End of list...
-    xbmcplugin.endOfDirectory(addon_handler, True)
+        xbmcplugin.setContent(addon_handler, 'episodes')
+        xbmcplugin.endOfDirectory(addon_handler, True)
 
 
-main()
+if __name__ == '__main__':
+    Main()
